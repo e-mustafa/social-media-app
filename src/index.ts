eacd490d9baf6935edbcb4fa1a614902d573e0af
e-mustafa <eng.mustafax@gmail.com>
2026-08-02 19:14:45 +0300

    add postman and github links

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { ENV } from './config/env.config';
 import { bootstrap } from './bootstrap';
 
 const app = express();
 
 bootstrap(app);
 
-
 export default app;
+
+
+//! 👈🏻👈🏻👈🏻👈🏻 <-- postman collection
+// https://documenter.getpostman.com/view/49016393/2sBY4Tpxq3
+
+//! 👈🏻👈🏻👈🏻👈🏻 <-- Github link
+// https://github.com/e-mustafa/social-media-app
+
+// npm run dev2
